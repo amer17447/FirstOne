14b7c01fbd665fb1e121b05570f74262d6af7e06
amer17447 <aamb17447@gmail.com>
2024-10-01 22:04:19 +0200

    TIZI3

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 System.out.println("Hallo World");        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        System.out.println("sex");
+
 
     }
 }
